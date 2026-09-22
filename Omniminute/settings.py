1ab3805255b805f0c410013d9d0de58b21375c3c
Shelomith Anyango <shelomith42@gmail.com>
2026-09-22 23:42:08 +0300

    feat: add CORS support for local development

--- Omniminute/settings.py
+++ Omniminute/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,9 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # Standard local Vite/React port
+    "http://127.0.0.1:5173",
+]
+    
